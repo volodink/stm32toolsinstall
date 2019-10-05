d0ccda86349f2d6e328886d5747312c79e0a9e09
Konstantin Volodin <volodin.konstantin@gmail.com>
2019-10-05 12:51:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ chmod +x install_sw4stm32_linux_64bits-v2.9.run
 unzip en.STM32CubeMX_5-2-1.zip -d STM32CubeMX5
 
 sudo apt install -y openjdk-8-jre
+sudo apt install -y cmake libusb-1.0 
+
+git clone https://github.com/texane/stlink
+cd stlink
+make release
+sudo make install
+cd build/Release/
+sudo make install
+sudo ldconfig

0a3b2e5ce8f0939e88848ca0b888b399bde99fae
Konstantin Volodin <volodin.konstantin@gmail.com>
2019-10-05 13:52:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ wget http://167.71.41.11/install_sw4stm32_linux_64bits-v2.9.run
 wget http://167.71.41.11/en.STM32CubeMX_5-2-1.zip
 
 chmod +x install_sw4stm32_linux_64bits-v2.9.run
+mkdir sw4stm32
+mv install_sw4stm32_linux_64bits-v2.9.run sw4stm32
+
 unzip en.STM32CubeMX_5-2-1.zip -d STM32CubeMX5
 
 sudo apt install -y openjdk-8-jre cmake libusb-1.0 gksu
